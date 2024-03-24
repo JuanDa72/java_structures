e5c9a66492643849c2bb08b0ef41dd028aba84cf
JuanDa72 <jcruzgi@unal.edu.co>
2024-03-23 19:32:58 -0500

    add linked list no complete

--- src/list/UnorderedLinkedList.java
+++ src/list/UnorderedLinkedList.java
@@ -22,6 +22,10 @@ public class UnorderedLinkedList<T> {
         return count==size;
     }
 
+    public int getCount(){
+        return count;
+    }
+
     public boolean pushFront(T key){
         boolean conditional=false;
         if (!isFull()){
@@ -37,6 +41,11 @@ public class UnorderedLinkedList<T> {
                 this.head=node;
                 conditional=true;
                 count++;
+                //GenericNode current=head;
+                //while(current.getNext()!=null){
+                //    current=current.getNext();
+                //}
+                //tail=current;
             }
         }
         return conditional;
@@ -46,12 +55,135 @@ public class UnorderedLinkedList<T> {
         return (T) head.getData();
     }
 
+    public T popFront(){
+        if (head==null){
+            System.err.println("Error: Linked list is empty");
+            throw new RuntimeException("Linked list is empty");
+        }
+        else{
+            GenericNode current=head;
+            head=head.getNext();
+            if (count==0){
+                tail=null;
+            }
+            count--;
+            return (T) current.getData();
+        }
+    }
+
+    public boolean pushBack(T key){
+        boolean inserted=false;
+        if(!isFull()) {
+            GenericNode node=new GenericNode(key);
+            if(tail==null){
+                head=tail=node;
+                count++;
+                inserted=true;
+        }
+            else {
+                tail.setNext(node);
+                tail = tail.getNext();
+                count++;
+                inserted=true;
+            }
+        }
+        return inserted;
+    }
+
+    public T keyTopBack(){
+        return (T) tail.getData();
+    }
+
+    public T popBack(){
+        GenericNode current=head;
+        if(head==null){
+            System.err.println("Error: linked list is empty");
+            throw new RuntimeException("Linked llist is empty");
+        }
+        else{
+            if (head==tail){
+                head=tail=null;
+                count--;
+                return (T) current.getData();
+            }
+            else{
+                while(current.getNext()!=null){
+                    current=current.getNext();
+                }
+                tail=current;
+                tail.setNext(null);
+                count--;
+                return (T) current.getData();
+            }
+        }
+    }
+
+    public boolean find(T key){
+        boolean found=false;
+        GenericNode current=head;
+        while(current!=null && !found){
+            if(current.getData()==key){
+                found=true;
+            }
+            else{
+                current=current.getNext();
+            }
+        }
+        return found;
+    }
+
+    public T erase(T key){
+        boolean deleted=false;
+        GenericNode current=head;
+        if(isEmpty()){
+            System.err.println("Error: linked list is empty");
+            throw new RuntimeException("linked list is empty");
+        }
+
+        else{
+            while(current!=null && !deleted){
+                if(current.getData()==key){
+                    if(current==head && current==tail){
+                        head=tail=null;
+                        count--;
+                    }
+                    else if(current==head  && current!=tail){
+                        head=current.getNext();
+                        count--;
+                    }
+                    else  if(current!=head && current==tail){
+                        GenericNode p=head;
+                        while(p.getNext()!=null){
+                            p=p.getNext();
+                        }
+                        tail=p;
+                        tail.setNext(null);
+                        count--;
+                    }
+
+                    deleted=true;
+                }
+                else{
+                    current=current.getNext();
+                }
+            }
+        }
+        return (T) current.getData();
+    }
+
 
 
     public static void main(String [] args){
         UnorderedLinkedList<Integer> linkedlist=new UnorderedLinkedList<>(4);
         System.out.println(linkedlist.pushFront(3));
         System.out.println(linkedlist.pushFront(2));
+        //System.out.println(linkedlist.popFront());
+        //System.out.println(linkedlist.pushBack(8));
+        linkedlist.pushBack(7);
+        linkedlist.pushBack(34);
+        System.out.println(linkedlist.find(2));
+        System.out.println(linkedlist.getCount());
+
     }
 
 }
