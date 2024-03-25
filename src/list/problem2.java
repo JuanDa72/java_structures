c6eb14e0196183191d5be8224c4f6671d0826e33
JuanDa72 <jcruzgi@unal.edu.co>
2024-03-25 17:27:57 -0500

    add problem 2 to list

--- src/list/problem2.java
+++ src/list/problem2.java
@@ -1,6 +1,30 @@
 package list;
+import list.OrderedArrayList;
+
+import java.util.Scanner;
 
 public class problem2 {
 
     public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int number_elements=sc.nextInt();
+        int position=sc.nextInt();
+
+        sc.nextLine();
+
+        String values=sc.nextLine();
+        String list[]=values.split(" ");
+        OrderedArrayList listJobs=new OrderedArrayList(list.length);
+
+        for (String element: list){
+            element="-"+element;
+            listJobs.insert(Integer.parseInt(element));
+        }
+
+        for(String element: list){
+            System.out.println(element);
+        }
+        System.out.println(listJobs);
+
     }
+}
