c19acd538e0ce5079a58bea6da4cbb48825f47cc
JuanDa72 <jcruzgi@unal.edu.co>
2024-04-17 19:01:01 -0500

    add bst insert method

--- src/trees/BinarySearchTree.java
+++ src/trees/BinarySearchTree.java
@@ -1,8 +1,6 @@
 package trees;
 import queue.QueueArrayGeneric;
 
-import javax.print.attribute.standard.Compression;
-
 public class BinarySearchTree <T> {
 
     private Node root;
@@ -18,11 +16,17 @@ public class BinarySearchTree <T> {
         private T key;
         private Node left;
         private Node right;
+        private Node parent;
 
         public Node(T key){
             this.key=key;
             left=null;
             right=null;
+            parent=null;
+        }
+
+        public String toString(){
+            return key.toString();
         }
 
         //Methods for Node class
@@ -33,6 +37,7 @@ public class BinarySearchTree <T> {
             return 0;
         }
         return 1+Math.max(height(node.left),height(node.right));
+        //return 1+Math.max(0,1);
     }
 
     public int height(){
@@ -107,23 +112,115 @@ public class BinarySearchTree <T> {
     }
 
     private Node find(Node node, T key){
-        if (node.key.compareTo(key)==0){
-            return node;
+        if(node==null) {
+            return null;
         }
-        else if(node.key.compareTo(key)<0){
-            return find(
-                    node.right,key);
+        else{
+            if (node.key.compareTo(key)==0){
+                return node;
+            }
+            else if(node.key.compareTo(key)<0){
+                return find(node.right,key);
+            }
+            else if(node.key.compareTo(key)>0){
+                return find(node.left,key);
+            }
+            else{
+                return null;
+            }
         }
-        else if(node.key.compareTo(key)>0){
-            return find(node.left,key);
-        }
-        else return null;
     }
 
     public Node find(T key){
+        if (root==null){
+            return null;
+        }
         return find(root, key);
     }
 
+    private Node leftDescendent(Node node){
+        if (node.left==null){
+            return node;
+        }
+        else{
+            return leftDescendent(node.left);
+        }
+    }
 
+    private Node rightAncestor(Node node){
+        if (node.key.compareTo(node.parent.key)<0){
+            return node.parent;
+        }
+        else{
+            return rightAncestor(node.parent);
+        }
+    }
+
+    private Node next(T key){
+        //Confiando en que el método find funcione correctamente
+        Node node=find(key);
+        if (node==null){
+            System.err.println("Error: Key not in tree");
+            throw new RuntimeException("Key not in tree");
+        }
+        else{
+            if (node.right!=null){
+                return leftDescendent(node.right);
+            }
+            else{
+                return rightAncestor(node);
+            }
+        }
+    }
+
+    //Implement of range search (on hold)
+
+    public Node insert(T key){
+        if(find(key)==null){
+            Node current=root;
+            if(root==null){
+                Node node=new Node((Comparable) key);
+                root=node;
+                return node;
+            }
+            while(current!=null){
+                if(current.key.compareTo(key)>0){
+                    if(current.left==null){
+                        Node node=new Node((Comparable) key);
+                        current.left=node;
+                        node.parent=current;
+                        return node;
+                    }
+                    else{
+                        current=current.left;
+                    }
+                }
+                else{
+                    if(current.right==null){
+                        Node node=new Node((Comparable) key);
+                        current.right=node;
+                        node.parent=current;
+                        return node;
+                    }
+                    else{
+                        current=current.right;
+                    }
+                }
+            }
+        }
+        System.err.println("Error: key is already in the tree");
+        throw new RuntimeException("Key is already in the tree");
+    }
+
+    public static void main(String []args){
+        BinarySearchTree<Integer> bst=new BinarySearchTree<>();
+        bst.insert(17);
+        bst.insert(8);
+        bst.insert(14);
+        bst.insert(3);
+        bst.insert(20);
+        bst.insert(5);
+        bst.postOrder();
+    }
 
 }
